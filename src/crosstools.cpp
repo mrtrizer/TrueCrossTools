2721294b318c34e4fb633f2c86870b10c491b7e8
Denis Zdorovtsov <mrtrizer@gmail.com>
2015-12-12 18:47:54 +0300

    Fixed compilation error.

--- src/crosstools.cpp
+++ src/crosstools.cpp
@@ -1,9 +1,11 @@
 #include "crosstools.h"
 
+#ifndef STM32F40_41xxx
 uint16_t __REV16(uint16_t value) //replacement for function from ST libs
 {
     return value << 8 | value >> 8;
 }
+#endif
 
 #ifndef __GXX_RTTI
 #warning "Dynamic cast is disabled. dynamic_casts are replaced with static_casts"
